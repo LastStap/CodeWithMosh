1a560d3e4a6c3cfe774db891a870656fe25d2b56
LastStap <dumshenko2003@gmail.com>
2026-02-02 11:02:40 +0100

    COD-13 Added Temperature Report

--- src/main/java/daniil/dumshenko/Main.java
+++ src/main/java/daniil/dumshenko/Main.java
@@ -3,8 +3,8 @@ package daniil.dumshenko;
 public class Main
 {
     public static void main( String[] args ) {
-        MortgageCalculator mortgageCalculator = new MortgageCalculator();
-        String result = mortgageCalculator.calculateMortgage();
-        System.out.println("Mortgage: " + result);
+        TemperatureReport temperatureReport = new TemperatureReport();
+        temperatureReport.temperatureReport();
+        System.out.println(temperatureReport);
     }
 }
